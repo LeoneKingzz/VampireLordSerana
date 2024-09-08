c8c6581b15846f097ce2072ed4adb046398ae7ac
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-09-08 19:07:13 +0100

    Update hook.cpp

--- src/hook.cpp
+++ src/hook.cpp
@@ -326,7 +326,7 @@ namespace hooks
 		dispelEffect(FXchange, a_actor);
 		caster->CastSpellImmediate(FXchange2, true, a_actor, 1, false, 0.0, a_actor);
 		dispelEffect(Gargoyle, a_actor);
-		a_actor->EvaluatePackage(true, true);
+		//a_actor->EvaluatePackage(true, true);
 		EquipfromInvent(a_actor, royal_armour->formID);
 		EquipfromInvent(a_actor, royal_boots->formID);
 		if (bElderScrollEquipped) {
