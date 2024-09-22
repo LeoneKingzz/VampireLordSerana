31d770a33e07ab0c2ed59c402c73afd49e4d3858
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-09-22 14:21:38 +0100

    Update hook.cpp

--- src/hook.cpp
+++ src/hook.cpp
@@ -175,7 +175,7 @@ namespace hooks
 				total_threat += it->threatValue;
 				continue;
 			}
-			if (total_threat <= 0.0375f) {
+			if (total_threat <= 0.1875f) {
 				adequate_threat = true;
 			}
 		}
